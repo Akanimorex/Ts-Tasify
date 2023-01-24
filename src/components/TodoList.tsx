6c70b4531ba4d6af4f8edf6011f7932e95d63f89
Akanimorex <akanimoime2016@gmail.com>
2023-01-24 15:27:28 +0100

    fixed the bug

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,18 +3,18 @@ import "./styles.css";
 import { Todo } from './model';
 
 interface Props {
-    todos:Todo[],
+    todos:Todo[];
     setToDos:React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
 
 
-const TodoList :React.FC =({ todos, setToDos}: Props)=>{
-    return(
+function TodoList({ todos, setToDos }: Props) {
+    return (
         <div className="todos">
 
         </div>
-    )
-};
+    );
+}
 
 export default TodoList;
